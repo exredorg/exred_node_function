999b52dc1c8a8f9e459f3b188eefedc0afb9a439
Zsolt <zsoltxyz@gmail.com>
2018-12-09 19:15:53 -0500

    need to use new nodeprototype

--- lib/exred_node_function.ex
+++ lib/exred_node_function.ex
@@ -1,12 +1,11 @@
 defmodule Exred.Node.Function do
   @moduledoc """
   Executes an arbitrary function to handle incoming messages.
-  
+
   ### WARNING: This is extremely unsecure. The function is executed as-is in the same beam VM as the rest of the application.
   There are no restricitions imposed on what this function can do.
   """
 
-  
   @name "Function"
   @category "function"
   @info @moduledoc
@@ -14,17 +13,16 @@ defmodule Exred.Node.Function do
     name: %{value: @name, type: "string"},
     code: %{value: "fn(msg, state)->\n\n\t{msg, state}\nend\n", type: "codeblock"}
   }
-  @ui_attributes %{right_icon: "build" }
-  
-  use Exred.Library.NodePrototype
+  @ui_attributes %{right_icon: "build"}
+
+  use Exred.NodePrototype
   require Logger
-  
+
   @impl true
   def handle_msg(msg, state) do
     {handler, _} = Code.eval_string(state.config.code.value)
     {newmsg, newstate} = handler.(msg, state)
-    Logger.debug("SENDING: #{inspect newmsg}")
+    Logger.debug("SENDING: #{inspect(newmsg)}")
     {newmsg, newstate}
   end
-
 end
